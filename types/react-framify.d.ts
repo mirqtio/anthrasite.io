ef7b0a17408813122eb5167db72f50b7a2829b78
mirqtio <mirqtio@thewellsocial.com>
2025-12-17 19:59:37 -0500

    fix: Add 'use client' to PurchaseHomepage to resolve Vercel build error

---
+++ types/react-framify.d.ts
@@ -0,0 +1,6 @@
+declare module 'react-framify' {
+    import React from 'react'
+
+    export const Iphone14Pro: React.FC<{ children: React.ReactNode }>
+    export const MacbookPro: React.FC<{ children: React.ReactNode }>
+}
